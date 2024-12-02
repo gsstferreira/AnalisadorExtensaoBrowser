60f7226dc9f04625db1ae7cfd2a13f968c9eaf31
gsstf <gustavo.shuiti@gmail.com>
2024-12-02 17:25:17 -0300

    Add project files.

---
+++ AnalisadorExtensaoBrowser/src/AnalisadorExtensaoBrowser/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Common.Classes;
+using System.Security.AccessControl;
+
+namespace AnalisadorExtensaoBrowser
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            var ext = ExtensaoBrowser.ObterExtensao(Constantes.urlTeste);
+            Console.WriteLine(ext.ToString());
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+                });
+            });
+        }
+    }
+}
---
+++ Common/Classes/Constantes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Common.Classes
+{
+    public class Constantes
+    {
+        public static string urlTeste = "https://chromewebstore.google.com/detail/browsergpt-seu-assistente/njggknpmkjapgklcfhaiigafiiebpchm?hl=pt-br";
+
+
+        public static string ChromeExtensionViewUrl = "https://chromewebstore.google.com/detail/";
+
+        public static string ChromeExtensionDownloadUrl = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=[PRODVER]&acceptformat=[FORMAT]&x=id%3D[ID]%26uc";
+
+        public static string ChromeProdVersion = "31.0.1609.0";
+        public static string AcceptedFormat = "crx2,crx3";
+
+    }
+}
